33d7adfded2737d4d52c9b896341f260f578cacd
cherrycita-dev <pereznancyesmeralda@gmail.com>
2026-01-19 23:35:23 -0600

    Add files via upload

---
+++ 4. Estructuras de seleccion/Interrogacion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Numero par o no
+
+    int main(){
+
+    int a;
+
+    puts("Ingrese un numero para comprobar si es par o no");
+    scanf("%i",&a);
+
+
+    (a%2==0) ? printf("Es par") : printf("Es impar");
+    return 0;
+
+    }
